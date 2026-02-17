cd444754a6d1ee982ec2b011494c6b6ffc70fb6e
Szekely Reka <szekely.reka.ext@kesz.hu>
2026-02-17 14:12:31 +0200

    implemented ConcreteProducts for Fancy+Modern

---
+++ AbstractFactory/Documents/FancyResume.cs
@@ -0,0 +1,26 @@
+﻿namespace AbstractFactory.Documents
+{
+    public class FancyResume : AbstractResume
+    {
+        public FancyResume(string style, string author, string company)
+        {
+            Style = style;
+            Author = author;
+            Company = company;
+        }
+
+        public override string Style { get; set; }
+        public override string Author { get; set; }
+        public override string Company { get; set; }
+
+        public override string ToString()
+        {
+            return "Resume: Style: " + Style + ", Author: " + Author + ", Company: " + Company + ", Date: " + Date;
+        }
+
+        public override void Interact(AbstractLetter abstractLetter)
+        {
+            Console.WriteLine(Style + " interacts with " + abstractLetter.Style);
+        }
+    }
+}
---
+++ AbstractFactory/Documents/ModernLetter.cs
@@ -0,0 +1,27 @@
+﻿namespace AbstractFactory.Documents
+{
+    public class ModernLetter : AbstractLetter
+    {
+        public ModernLetter(string title, string style, string author)
+        {
+            Title = title;
+            Style = style;
+            Author = author;
+        }
+
+        public override string Title { get; set; }
+        public override string Style { get; set; }
+        public override string Author { get; set; }
+
+        public override void Interact(AbstractResume abstractResume)
+        {
+            Console.WriteLine(Style + " interacts with " + abstractResume.Style);
+        }
+
+        public override string ToString()
+        {
+            return "Letter: Style: " + Style + ", Author: " + Author + ", Title: " + Title + ", Date: " + Date;
+        }
+
+    }
+}
